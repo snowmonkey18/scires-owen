544079f1f24c386d7f59db4f2cc2ea1f3d7d9d32
snowmonkey18 <snowmonkey.gt@gmail.com>
2020-08-15 17:01:13 -0400

    Graph wireframe Owen's T function on both 'restricted' and 'full' domains

---
+++ graph_owen_plotly_wireframe_full.py
@@ -0,0 +1,43 @@
+import chart_studio.plotly as py
+import plotly.graph_objs as go
+import numpy as np
+
+# Creating the data
+x = np.linspace(-6.3,6.3,63)
+y = np.linspace(-10,10,50)
+xGrid, yGrid = np.meshgrid(x,y)
+z = special.owens_t(xGrid,yGrid)
+
+# Creating the plot
+lines = []
+line_marker = dict(color='#0066FF', width=2)
+for i, j, k in zip(xGrid, yGrid, z):
+    lines.append(go.Scatter3d(x=i, y=j, z=k, mode='lines', line=line_marker))
+
+layout = go.Layout(
+    title='Wireframe Plot',
+    scene=dict(
+        xaxis=dict(
+            gridcolor='rgb(255, 255, 255)',
+            zerolinecolor='rgb(255, 255, 255)',
+            showbackground=True,
+            backgroundcolor='rgb(230, 230,230)'
+        ),
+        yaxis=dict(
+            gridcolor='rgb(255, 255, 255)',
+            zerolinecolor='rgb(255, 255, 255)',
+            showbackground=True,
+            backgroundcolor='rgb(230, 230,230)'
+        ),
+        zaxis=dict(
+            gridcolor='rgb(255, 255, 255)',
+            zerolinecolor='rgb(255, 255, 255)',
+            showbackground=True,
+            backgroundcolor='rgb(230, 230,230)'
+        )
+    ),
+    showlegend=False,
+)
+fig = go.Figure(data=lines, layout=layout)
+plotly.offline.iplot(fig)
+#py.iplot(fig, filename='wireframe_plot')
